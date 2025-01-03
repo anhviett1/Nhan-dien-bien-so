7dbd85fc40a570fd911bd69257d9b09ecac62b28
Anh Nguyen <vietanh32sl@gmail.com>
2025-01-03 10:29:19 +0700

    model luu data

--- LicensePlateRecognition/plates/models.py
+++ LicensePlateRecognition/plates/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class  LicencePlate(models.Model):
+    plate_number = models.CharField(max_length=15, unique=True)
+    detected_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.plate_number
